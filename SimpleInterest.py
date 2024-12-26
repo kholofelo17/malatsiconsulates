daf8c4dce63af05463cfb5597f6d5cdd2e634f38
kholofelo17 <kholo55@gmail.com>
2024-12-26 15:09:00 +0200

    Create SimpleInterest.py

---
+++ SimpleInterest.py
@@ -0,0 +1,7 @@
+def simple_interest(principal, rate , time):
+  return principal * rate * time / 100
+
+# Example usage:
+p, r, t = 1000, 5, 2
+print(f"Simple Interest: {simple_interest(p, r, t)}")
+```
